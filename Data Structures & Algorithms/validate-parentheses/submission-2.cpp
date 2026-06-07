d7a5df237411cb5b27f77b4c411512a2e7602997
Danny Wang <101087210+danny0926@users.noreply.github.com>
2026-06-07 11:48:52 +0800

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,20 @@
+#include<stack>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(auto& c: s){
+            if(c == '(' || c == '{' || c == '[') stk.push(c);
+            else{
+                if (stk.empty()) return false;
+                if (c == ')' && stk.top() != '(') return false;
+                if (c == '}' && stk.top() != '{') return false;
+                if (c == ']' && stk.top() != '[') return false;
+                stk.pop();
+            }
+        }
+        if (!stk.empty()) return false;
+        return true;
+    }
+};
